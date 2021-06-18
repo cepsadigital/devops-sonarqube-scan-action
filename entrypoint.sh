e50550635a42899befa1e6d6b4aac4ff94529716
Alberto Cantalejo Sancho <70366199+acantalejo-keepler@users.noreply.github.com>
2021-06-18 13:51:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+BRANCH_FULL=${GITHUB_REF}
+BRANCH=${BRANCH_FULL/refs\/heads\//""}
+
 if [[ "${INPUT_CHECKQUALITYGATE}" == "true" ]]; then
 	sonar-scanner \
 		-Dsonar.host.url=${INPUT_HOST} \
     		-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
     		-Dsonar.login=${INPUT_LOGIN} \
     		-Dsonar.password=${INPUT_PASSWORD} \
+		-Dsonar.branch.name=${BRANCH} \
 		-Dsonar.qualitygate.wait \
 		-Dsonar.qualitygate.timeout=300
 else
@@ -14,5 +18,5 @@ else
     		-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
     		-Dsonar.login=${INPUT_LOGIN} \
     		-Dsonar.password=${INPUT_PASSWORD} \
-		-Dsonar.branch.name=develop
+		-Dsonar.branch.name=${BRANCH}
 fi
